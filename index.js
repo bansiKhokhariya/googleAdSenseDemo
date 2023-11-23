231688edca0bad9a10cf8e4c733b9c24c5e76a79
bansi-hkapps <bansi.hkapps@gmail.com>
2023-11-23 10:56:46 +0530

    add html food code

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views'); // Optional: specify the directory for your views
+// // Set EJS as the view engine
+// app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views'); // Optional: specify the directory for your views
 
-// Your other routes and middleware go here
+// // Your other routes and middleware go here
 
-app.get('/', (req, res) => {
-    // Fetch and pass the AdSense code to the view
-    // const adSenseCode = '...'; // Replace with your actual AdSense code
-    // res.render('index', { adSenseCode });
-    res.render('index');
-});
+// app.get('/', (req, res) => {
+//     // Fetch and pass the AdSense code to the view
+//     // const adSenseCode = '...'; // Replace with your actual AdSense code
+//     // res.render('index', { adSenseCode });
+//     res.render('index');
+// });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
